e352fba25ae904921ab671abebe43ea8556b152c
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2019-04-24 09:55:21 +0200

    more debug log

---
+++ android/build/generated/jni/de.appwerft.watchdog.WatchdogModule.cpp
@@ -0,0 +1,280 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011-2018 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+/** This code is generated, do not edit by hand. **/
+
+#include "de.appwerft.watchdog.WatchdogModule.h"
+
+#include "AndroidUtil.h"
+#include "JNIUtil.h"
+#include "JSException.h"
+#include "TypeConverter.h"
+#include "V8Util.h"
+
+
+
+
+#include "org.appcelerator.kroll.KrollModule.h"
+
+#define TAG "WatchdogModule"
+
+using namespace v8;
+
+namespace de {
+namespace appwerft {
+namespace watchdog {
+
+
+Persistent<FunctionTemplate> WatchdogModule::proxyTemplate;
+jclass WatchdogModule::javaClass = NULL;
+
+WatchdogModule::WatchdogModule() : titanium::Proxy()
+{
+}
+
+void WatchdogModule::bindProxy(Local<Object> exports, Local<Context> context)
+{
+	Isolate* isolate = context->GetIsolate();
+
+	Local<FunctionTemplate> pt = getProxyTemplate(isolate);
+
+	v8::TryCatch tryCatch(isolate);
+	Local<Function> constructor;
+	MaybeLocal<Function> maybeConstructor = pt->GetFunction(context);
+	if (!maybeConstructor.ToLocal(&constructor)) {
+		titanium::V8Util::fatalException(isolate, tryCatch);
+		return;
+	}
+
+	Local<String> nameSymbol = NEW_SYMBOL(isolate, "Watchdog"); // use symbol over string for efficiency
+	MaybeLocal<Object> maybeInstance = constructor->NewInstance(context);
+	Local<Object> moduleInstance;
+	if (!maybeInstance.ToLocal(&moduleInstance)) {
+		titanium::V8Util::fatalException(isolate, tryCatch);
+		return;
+	}
+	exports->Set(context, nameSymbol, moduleInstance);
+}
+
+void WatchdogModule::dispose(Isolate* isolate)
+{
+	LOGD(TAG, "dispose()");
+	if (!proxyTemplate.IsEmpty()) {
+		proxyTemplate.Reset();
+	}
+
+	titanium::KrollModule::dispose(isolate);
+}
+
+Local<FunctionTemplate> WatchdogModule::getProxyTemplate(v8::Isolate* isolate)
+{
+	Local<Context> context = isolate->GetCurrentContext();
+	if (!proxyTemplate.IsEmpty()) {
+		return proxyTemplate.Get(isolate);
+	}
+
+	LOGD(TAG, "WatchdogModule::getProxyTemplate()");
+
+	javaClass = titanium::JNIUtil::findClass("de/appwerft/watchdog/WatchdogModule");
+	EscapableHandleScope scope(isolate);
+
+	// use symbol over string for efficiency
+	Local<String> nameSymbol = NEW_SYMBOL(isolate, "Watchdog");
+
+	Local<FunctionTemplate> t = titanium::Proxy::inheritProxyTemplate(
+		isolate,
+		titanium::KrollModule::getProxyTemplate(isolate),
+		javaClass,
+		nameSymbol);
+
+	proxyTemplate.Reset(isolate, t);
+	t->Set(titanium::Proxy::inheritSymbol.Get(isolate), FunctionTemplate::New(isolate, titanium::Proxy::inherit<WatchdogModule>));
+
+	// Method bindings --------------------------------------------------------
+	titanium::SetProtoMethod(isolate, t, "stop", WatchdogModule::stop);
+	titanium::SetProtoMethod(isolate, t, "start", WatchdogModule::start);
+
+	Local<ObjectTemplate> prototypeTemplate = t->PrototypeTemplate();
+	Local<ObjectTemplate> instanceTemplate = t->InstanceTemplate();
+
+	// Delegate indexed property get and set to the Java proxy.
+	instanceTemplate->SetIndexedPropertyHandler(titanium::Proxy::getIndexedProperty,
+		titanium::Proxy::setIndexedProperty);
+
+	// Constants --------------------------------------------------------------
+
+	// Dynamic properties -----------------------------------------------------
+
+	// Accessors --------------------------------------------------------------
+
+	return scope.Escape(t);
+}
+
+Local<FunctionTemplate> WatchdogModule::getProxyTemplate(v8::Local<v8::Context> context)
+{
+	return getProxyTemplate(context->GetIsolate());
+}
+
+// Methods --------------------------------------------------------------------
+void WatchdogModule::stop(const FunctionCallbackInfo<Value>& args)
+{
+	LOGD(TAG, "stop()");
+	Isolate* isolate = args.GetIsolate();
+	Local<Context> context = isolate->GetCurrentContext();
+	HandleScope scope(isolate);
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		titanium::JSException::GetJNIEnvironmentError(isolate);
+		return;
+	}
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(WatchdogModule::javaClass, "stop", "()V");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'stop' with signature '()V'";
+			LOGE(TAG, error);
+				titanium::JSException::Error(isolate, error);
+				return;
+		}
+	}
+
+	Local<Object> holder = args.Holder();
+	if (!JavaObject::isJavaObject(holder)) {
+		holder = holder->FindInstanceInPrototypeChain(getProxyTemplate(isolate));
+	}
+	if (holder.IsEmpty() || holder->IsNull()) {
+		LOGE(TAG, "Couldn't obtain argument holder");
+		args.GetReturnValue().Set(v8::Undefined(isolate));
+		return;
+	}
+	titanium::Proxy* proxy = NativeObject::Unwrap<titanium::Proxy>(holder);
+	if (!proxy) {
+		args.GetReturnValue().Set(Undefined(isolate));
+		return;
+	}
+
+	jvalue* jArguments = 0;
+
+
+	jobject javaProxy = proxy->getJavaObject();
+	if (javaProxy == NULL) {
+		args.GetReturnValue().Set(v8::Undefined(isolate));
+		return;
+	}
+	env->CallVoidMethodA(javaProxy, methodID, jArguments);
+
+	proxy->unreferenceJavaObject(javaProxy);
+
+
+
+	if (env->ExceptionCheck()) {
+		titanium::JSException::fromJavaException(isolate);
+		env->ExceptionClear();
+	}
+
+
+
+
+	args.GetReturnValue().Set(v8::Undefined(isolate));
+
+}
+void WatchdogModule::start(const FunctionCallbackInfo<Value>& args)
+{
+	LOGD(TAG, "start()");
+	Isolate* isolate = args.GetIsolate();
+	Local<Context> context = isolate->GetCurrentContext();
+	HandleScope scope(isolate);
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		titanium::JSException::GetJNIEnvironmentError(isolate);
+		return;
+	}
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(WatchdogModule::javaClass, "start", "(Lorg/appcelerator/kroll/KrollDict;)V");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'start' with signature '(Lorg/appcelerator/kroll/KrollDict;)V'";
+			LOGE(TAG, error);
+				titanium::JSException::Error(isolate, error);
+				return;
+		}
+	}
+
+	Local<Object> holder = args.Holder();
+	if (!JavaObject::isJavaObject(holder)) {
+		holder = holder->FindInstanceInPrototypeChain(getProxyTemplate(isolate));
+	}
+	if (holder.IsEmpty() || holder->IsNull()) {
+		LOGE(TAG, "Couldn't obtain argument holder");
+		args.GetReturnValue().Set(v8::Undefined(isolate));
+		return;
+	}
+	titanium::Proxy* proxy = NativeObject::Unwrap<titanium::Proxy>(holder);
+	if (!proxy) {
+		args.GetReturnValue().Set(Undefined(isolate));
+		return;
+	}
+
+
+	jvalue jArguments[1];
+
+
+
+
+	bool isNew_0;
+	if (args.Length() <= 0) {
+		jArguments[0].l = NULL;
+
+	} else {
+	if (!args[0]->IsNull()) {
+		Local<Value> arg_0 = args[0];
+		jArguments[0].l =
+			titanium::TypeConverter::jsObjectToJavaKrollDict(
+				isolate,
+				env, arg_0, &isNew_0);
+	} else {
+		jArguments[0].l = NULL;
+	}
+	}
+
+
+	jobject javaProxy = proxy->getJavaObject();
+	if (javaProxy == NULL) {
+		args.GetReturnValue().Set(v8::Undefined(isolate));
+		return;
+	}
+	env->CallVoidMethodA(javaProxy, methodID, jArguments);
+
+	proxy->unreferenceJavaObject(javaProxy);
+
+
+
+			if (isNew_0) {
+				env->DeleteLocalRef(jArguments[0].l);
+			}
+
+
+	if (env->ExceptionCheck()) {
+		titanium::JSException::fromJavaException(isolate);
+		env->ExceptionClear();
+	}
+
+
+
+
+	args.GetReturnValue().Set(v8::Undefined(isolate));
+
+}
+
+// Dynamic property accessors -------------------------------------------------
+
+
+} // watchdog
+} // appwerft
+} // de
